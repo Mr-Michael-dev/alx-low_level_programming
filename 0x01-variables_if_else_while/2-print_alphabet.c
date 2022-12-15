5d689a357663438eb247c3ba312dc4bc5e1b6aec
Michael <michael.oyedepo@gmail.com>
2022-12-14 23:40:18 -0800

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Use `putchar` function to print alphabet in lowercase.
+ * in new line
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	putchar('\n');
+	}
+
+	return (0);
+}
