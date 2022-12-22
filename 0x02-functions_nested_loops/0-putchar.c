097f217ce4ca593608f9a8a573650b14917f7d04
Michael <michael.oyedepo@gmail.com>
2022-12-22 01:43:56 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry Point.use only user defined variable to create output.
+ *
+ * Description: write a program that Print _putchar followed by a new line.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar("_putchar");
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains solutions to my alx project on functions and nested loops.
