38d1e97fbed9ca4b1e521c4f9640dcd32a64b954
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-09-08 13:34:25 -0700

    correct error in betty documentation style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,17 +2,16 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  * @n is a variable of type int
  * @n_last variable of type int, stores last digit of n
  *
  * Description: This program will assign a random number
  * to the variable n each time it is executed.
- * The last digit of n is extrated using '%10'
- * and stored in another variable n_last.
  * It uses if_else to check whether the number stored in the variable n_last
  * is greater than or less than 6 and not 0 or is 0
+ *
  * Return: Always 0 (exit the program)
  */
 
@@ -32,7 +31,7 @@ int main(void)
 {
 	printf("Last digit of %d is %d and is greater than 5\n", n, n_last);
 }
-	else if (n_last < 6 && n_last > 0)
+	else if (n_last < 6 && n_last != 0)
 {
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n_last);
 }
