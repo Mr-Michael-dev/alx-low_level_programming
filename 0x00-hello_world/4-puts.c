9906121f075867f73698e6fd188a0cec42ed2886
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-09-07 18:34:03 -0700

    Write a C program that prints strings of text using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return: 0 (exits on success)
+ */
+
+int main(void)
+{
+	const char *program = "Programming is like building a multilingual puzzle";
+
+	puts(program);
+
+	return (0);
+}
