414d764e61d5a35f3ee5b54e10edaae60ae4ac09
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-09-07 21:37:36 -0700

    Write a C program that prints some text using printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - entry point
  *
- * return: 0 (exits on success)
+ * Return: Always  0 (exits on success)
  */
 
 int main(void)
 {
-	const char *program = "Programming is like building a multilingual puzzle";
+	const char *program = "\"Programming is like building a multilingual puzzle";
 
 	puts(program);
 
