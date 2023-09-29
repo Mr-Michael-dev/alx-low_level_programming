528a6ff4cc685aac16b0e8ac51fe62fdf13802f0
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-09-29 06:44:20 -0700

    Write a function that returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,18 +1,44 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural square rooy of a number
+ * is_prime_number - returns 1 of n is prime number otherwise 0
  * @n: number to be checked
+ * @denominator: number to divide the guess
  *
  * Return: sqrt
  */
 
-int _sqrt_recursion(int n)
+int _is_prime_helper(int n, int denominator);
+
+int is_prime_number(int n)
 {
-	if (n < 0)
+	if (n <= 1)
 	{
 		return (0);
 	}
-	
-	return (n / _sqrt_recursion(n - 1));
+
+	return (_is_prime_helper(n, 2));
+}
+
+
+/**
+ * _is_prime_helper - helper function to checks if n is prime
+ * @n: number to be check
+ * @denominator: number to divide the guess
+ *
+ * Return: recursive
+ */
+
+int _is_prime_helper(int n, int denominator)
+{
+	if (denominator * denominator > n)
+	{
+		return (1);
+	}
+	if (n % denominator == 0)
+	{
+		return (0);
+	}
+
+	return (_is_prime_helper(n, denominator + 1));
 }
