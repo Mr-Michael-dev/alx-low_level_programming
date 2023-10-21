dbd6519d01f01605bc4b65e06a0631a2ce3b323a
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-10-21 00:33:13 -0700

    write a function wqthat prints elements in a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the element of a list
+ * @h: pointer to list
+ *
+ * Return: number of nodes
+ */
+
+
+size_t print_list(const list_t *h)
+{
+	size_t el_count;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		
+		el_count++;
+	}
+	return (el_count);
+}
