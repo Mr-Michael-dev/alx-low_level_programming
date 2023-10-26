7770c56a0c979e6932481fecaba1044fde1da311
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-10-26 15:35:31 -0700

    create README

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define AND '&'
+#define OR '|'
+#define XOR '^'
+#define NOT '~'
+#define SHIFT_RIGHT '>>'
+#define SHIFT_LEFT '<<'
+
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_H */
