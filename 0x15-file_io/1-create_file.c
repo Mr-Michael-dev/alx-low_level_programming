cde7daf422e8df1eba572541435f52b1e5212aac
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-10-30 17:14:56 -0700

    Create a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,26 +14,27 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int length = strlen(text_content);
 
-	if (filename == NULL)
+	if (filename != NULL)
 	{
-		return (-1);
+		fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+
+		if (fd == -1)
+		{
+			return (-1);
+		}
 	}
-
-
-	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
-
-	if (fd == -1)
+	else
 	{
 		return (-1);
 	}
 
 	if (text_content != NULL)
 	{
-	if ((write(fd, text_content, length)) != length)
-	{
-		close(fd);
-		return (-1);
-	}
+		if ((write(fd, text_content, length)) != length)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
