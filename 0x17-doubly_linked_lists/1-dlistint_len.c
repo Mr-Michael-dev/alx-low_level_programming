f6ab79c90fc03d46f69bc69a19f0e007f1ce1fce
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-12-07 19:57:22 +0100

    Get List length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - returns number of elements of a d-list
+ * @h: head of the doubly linked list
+ *
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count_nodes = 0;
+
+	while (h != NULL)
+	{
+		count_nodes += 1;
+		h = h->next;
+	}
+
+	return (count_nodes);
+}
