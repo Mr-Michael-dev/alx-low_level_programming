4b383318720fb8cd2d409c8c842ebcecf3a833a4
Mr-Michael_dev <michael.oyedepo@gmail.com>
2023-12-08 06:01:23 +0100

    debug

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < index; i++)
+	for (i = 0; i < index && head != NULL; i++)
 	{
 		head = head->next;
 	}
